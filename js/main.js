206943c78308b9a6f072c4a616f07a542873d4c1
Simon <olav@live.se>
2022-11-23 14:55:19 +0100

    basic html

--- js/main.js
+++ js/main.js
@@ -4,6 +4,15 @@ import * as api from './components/apiRequests.js';
 import * as buttonHandler from './components/buttonHandler.js';
 import * as domModifier from './components/domModifier.js';
 
+let searchBtn = document.querySelector(".searchBtn");
+
+searchBtn.addEventListener("click", function(){
+
+    buttonHandler.search();
+
+})
+
+
 //Hämtar från apiRequests.js
 // console.log(api.getPlanets());
 // console.log(api.getPersons());
