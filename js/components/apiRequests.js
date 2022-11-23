b5c4b6b4cf63619ddd38ec6bfcc9fa3cdb910095
Simon <olav@live.se>
2022-11-23 11:21:43 +0100

    init

--- js/components/apiRequests.js
+++ js/components/apiRequests.js
@@ -1,6 +1,10 @@
 // Här hämtar vi data ifrån Swapi och skickar det tillbaka med en return funktion
-export function hello(){
 
-    return "Hello";
-    
-}
+// export let secret = "Hemligt";
+// export function getPlanets(){
+//     return "Planet funktion";
+// }
+// export function getPersons(){
+//     return "Person funktion";
+// }
+
