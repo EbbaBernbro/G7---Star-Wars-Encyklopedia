c95ea44e175bb0e1dc70ad1d6ea465ba68ac668d
Simon <olav@live.se>
2022-11-23 11:21:50 +0100

    init

--- js/components/buttonHandler.js
+++ js/components/buttonHandler.js
@@ -1,4 +1,7 @@
 // Tar emot inputs ifrån sidan, skickar de vidare
 
+// export function clickedButton(){
+//     return "Du tryckte på knappen";
+// }
 
 let searchBtn = document.querySelector("#searchBtn");
--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
 //Grundfilen som hanterar all kommunikation mellan filerna
 
-import {hello} from 'components/apiRequests.js';
-console.log(hello());
+import * as api from './components/apiRequests.js';
+import * as buttonHandler from './components/buttonHandler.js';
+import * as domModifier from './components/domModifier.js';
+
+//Hämtar från apiRequests.js
+// console.log(api.getPlanets());
+// console.log(api.getPersons());
+//Hämtar från buttonhandler.js
+// console.log(buttonHandler.clickedButton());
+// console.log(api.secret);
+
+
 
